fe1b88078020412d926478f380cf84f1b7129551
Mathias0777 <almeidamanoel0677@gmail.com>
2026-06-01 20:24:37 -0300

    verificar_sinal.js

---
+++ verificar_sinal.js
@@ -0,0 +1,9 @@
+let valor = -7;
+
+if (valor > 0) {
+    console.log("O numero e POSITIVO");
+} else if (valor < 0) {
+    console.log("O numero e NEGATIVO");
+} else {
+    console.log("O numero e ZERO");
+}
