5bbaa732c4e0e8dd412d81554092b950478a32c7
Mathias0777 <almeidamanoel0677@gmail.com>
2026-06-01 20:23:25 -0300

    maior_numero.js

---
+++ maior_numero.js
@@ -0,0 +1,11 @@
+let num1 = 25;
+let num2 = 42;
+
+if (num1 > num2) {
+    console.log("O maior numero e: " + num1);
+} else if (num2 > num1) {
+    console.log("O maior numero e: " + num2);
+} else {
+    console.log("Os numeros sao iguais");
+}
+
